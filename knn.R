64705abee3aa16a754aa74c95c75afcb722567a9
Settarov Ernest <39336766+TIR13@users.noreply.github.com>
2019-10-07 09:21:52 +0300

    Update knn.R

--- knn.R
+++ knn.R
@@ -73,4 +73,35 @@ xl <- iris[, 3:5]
 
 class <- kNN(xl, z, k=6) 
 
-points(z[1], z[2], pch = 22, bg = colors[class], asp = 1) 
+points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
+
+Loo <- function(k,xl)
+
+   {
+    sum =0
+    for(i in 1:dim(xl)[1])
+       {
+        tmpXL <- rbind(xl[1:i-1, ],
+        xl[i+1:dim(xl)[1],])
+        xi <- c(xl[i,1], xl[i,2])
+        class <-kNN(tmpXL,xi,k)
+        if(class != xl[i,3])
+        sum=sum+1
+       }
+   sum=sum/dim(xl)[1]
+   return(sum)
+  }
+y <- rep(0,150)
+kk <- 0
+min <- 150
+for(k in 1:150){
+x <- c(1:150)
+y[k] <- Loo(k,xl)
+if(y[k]<min){
+min <- y[k]
+kk <- k
+}
+}
+plot(x,y,type="l")
+label = paste("   K = ", kk, "\n", "   LOO = ", round(y[kk],3), sep = "")
+text(x[kk],y[kk],labels=label,pos=4) 
