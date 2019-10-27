c60ba830ee520f3c297554dbc858ef7e89e9b0ac
TIR13 <ernest_star1313@mail.ru>
2019-10-27 22:38:30 +0300

    update fris

--- metrica/fris_stolp.R
+++ metrica/fris_stolp.R
@@ -6,6 +6,8 @@ dis = function(u, v)
   return (sqrt(sum((u - v)^2)))
 }
 
+
+
 NN <- function(u,U){
 
 	n <- dim(U)[1]
@@ -16,7 +18,7 @@ NN <- function(u,U){
 		x=U[i,1:2]
 		
 		if(u[1]!=x[1] & u[2] != x[2]){
-			k <- dis(u,x)
+			k <- dis(as.double(u),as.double(x))
 			
 			if(k < min){
 				min <- k
@@ -30,13 +32,38 @@ NN <- function(u,U){
 }
 
 
+gaus <- function(r){
+	return (((2*pi)^(-1/2)) * exp(-1/2*r^2))
+}
 
 firis <- function(a,u,b){
+a=as.double(a)
+b=as.double(b)
+u=as.double(u)
 	S <- (dis(a,b)-dis(a,u))/(dis(a,b)+dis(a,u))
 	
 	return(S)
 }
+
+parzen <- function(xl,y,h,map)
+{
+	n <- dim(xl)[1]
+	weights = rep(0,3)
+	names(weights) = c("setosa", "versicolor", "virginica")
+	for(i in 1:n)
+	{
+	    x <- xl[i,1:2]
+	    Xy <- xl[(which(xl[,3]!=xl[i,3])),1:3]
+	    class <- xl[i,3]
+	    r <- firis(y,x,NN(y,Xy))/h
+	    weights[class] <- gaus(r)+weights[class]
+	}
+	class <- names(which.max(weights))
 	
+    		return(class) 
+
+}
+
 FindEtalon <- function(xl,xy,Etalone){
 	n <- dim(xy)[1]
 	nn <- dim(xl)[1]
@@ -55,6 +82,8 @@ FindEtalon <- function(xl,xy,Etalone){
 		sum2 <- 0
 		
 		for (j in 1:nn){
+			
+	print(xy)
 			if(xy[i,3]==xl[j,3] & xy[i,1]!=xl[j,1] & xy[i,2]!=xl[j,2]){
 				sum1 <- sum1+firis(xl[j,1:2],x[i,],NN(xl[j,1:2],Etalone))
 			}
@@ -75,6 +104,8 @@ FindEtalon <- function(xl,xy,Etalone){
 	
 	return(xy[k,1:3])
 }
+
+
 
 frisstolp <- function(xxl){
 	#xxl=unique(iris[,3:5])
@@ -159,24 +190,17 @@ frisstolp <- function(xxl){
 	return(Etalone)
 }
 
-#lx <- iris[,3:5]
+lx <- iris[,3:5]
 Etalone <- frisstolp(lx)
 
-z <- c(1.45,0.2)
 
-plot(lx[,1:2],bg = colors[lx[,3]], pch = 21, asp = 1,main = "FRIS STOLP", ylab = "y ", xlab = "x", col.lab = "red")
-class <- parzen(Etalone,z,1,TRUE)
-points(z[1],z[2],pch=0,col=colors[class])
+plot(Etalone[,1:2],bg = colors[Etalone[,3]], pch = 21, asp = 1,main = "FRIS ", ylab = "y ", xlab = "x", col.lab = "red")
 
-plot(lx[,1:2],bg = colors[lx[,3]], pch = 21, asp = 1,main = "STOLP", ylab = "y ", xlab = "x", col.lab = "red")
-class <- parzen(lx,z,1,TRUE)
-points(z[1],z[2],pch=0,col=colors[class])
-
-for(i in seq(1.30, 1.55, 0.01)){
-	for(j in seq(0.05,0.3,0.01)){
+for(i in seq(1, 7, 0.1)){
+	for(j in seq(-1,4,0.1)){
 	
 	    z <- c(i, j)
-	    class <- parzen(Etalone,z,1,TRUE)
+	    class <- parzen(iris[,3:5],z,1,TRUE)
 	    points(z[1], z[2], pch = 1,col=colors[class])
 	}
 }
