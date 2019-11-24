07489a8e8b9597016e25ccb6d075b3576d46d84e
Settarov Ernest <39336766+TIR13@users.noreply.github.com>
2019-11-24 13:28:57 +0300

    Update plug-in.R

--- bayes/plug-in.R
+++ bayes/plug-in.R
@@ -31,21 +31,24 @@ get_matrix <- function(xl,mu)
 
 }
 
-coef <- function(mu1,mu2,sigma1,sigma2)
+get_coef <- function(mu1,mu2,sigma1,sigma2)
 {
+	determ1 <-det(sigma1)
+  	determ2 <-det(sigma2)
 	  
-	invsigma1 <- solve(sigma1)
-	invsigma2 <- solve(sigma2)
-	a <- invsigma1 - invsigma2
-	A <- a[1,1]
-	B <- a[2,2]
-	C <- 2*a[1,2]
-	
-	b <- invsigma1%*%t(mu1) - invsigma2%*%t(mu2)
-	
-	D <- -2*b[1,1]
-	E <- -2*b[2,1]
-	F <- c(log(det(sigma1)) - log(det(sigma2)) + mu1%*%invsigma1%*%t(mu1) - mu2%*%invsigma2%*%t(mu2))
+	a <- c(sigma1[2,2]/determ1,sigma2[2,2]/determ2)
+  	b <- c(-sigma1[2,1]/determ1,-sigma2[2,1]/determ2)
+  	c <- c(-sigma1[1,2]/determ1,-sigma2[1,2]/determ2)
+  	d <- c(sigma1[1,1]/determ1,sigma2[1,1]/determ2)
+
+	A <- a[1]-a[2]
+  	B <- d[1]-d[2]
+  	C <- b[1]+c[1]+b[2]+c[2]
+  	D <- -2*mu1[1]*a[1]-2*mu1[2]*b[1]-mu1[1]*c[1]+2*mu2[1]*a[2]+b[2]*mu2[1]+mu2[2]*c[2]
+  	E <- -mu1[1]*b[1]-mu1[1]*c[1]-d[1]*2*mu1[2]+b[2]*mu2[1]+c[2]*mu2[1]+2*mu2[2]*d[2]
+	F <-  -log(abs(determ1)) + log(abs(determ2)) + 
+		mu1[1]*mu1[1]*a[1]+(b[1]+c[1])*mu1[1]*mu1[2]+d[1]*mu1[2]*mu1[2]-
+		mu2[1]*mu2[1]*a[2]-(b[2]+c[2])*mu2[1]*mu2[2]-d[2]*mu2[2]*mu2[2]
 	
 	func <- function(x, y) {
 		x^2*A + y^2*B + x*y*C + x*D + y*E + F
@@ -58,35 +61,32 @@ coef <- function(mu1,mu2,sigma1,sigma2)
 
 n <- 100
 
-sigma1 <- matrix(c(1,0, 0, 1), 2, 2)
-sigma2 <- matrix(c(1, 0,0, 1), 2, 2)
+sigma1 <- matrix(c(10,0, 0, 1), 2, 2)
+sigma2 <- matrix(c(1, 0,0, 5), 2, 2)
 
-mu1 <- c(5, 5)
-mu2 <- c(15, 15)
+mu1 <- c(1, 0)
+mu2 <- c(15, 0)
 
 xy1 <- mvrnorm(n=n, mu = mu1, Sigma = sigma1)
 xy2 <- mvrnorm(n=n, mu = mu2, Sigma = sigma2)
 
-plotxmin <- min(xy1[,1], xy2[,1]) - 1
-plotymin <- min(xy1[,2], xy2[,2]) - 1
-plotxmax <- max(xy1[,1], xy2[,1]) + 1
-plotymax <- max(xy1[,2], xy2[,2]) + 1
-plot(xy1[,1],xy2[,2], type="n", xlab = "x", ylab = "y", xlim=c(plotxmin, plotxmax), ylim = c(plotymin, plotymax))
+xl <- rbind(cbind(xy1, 1), cbind(xy2, 2))
 
+# Рисуем обучающую выборку
 colors <- c("gray", "orange")
-points(xy1, pch=21, col=colors[1], bg=colors[1])
-points(xy2, pch=21, col=colors[2], bg=colors[2])
+plot(xl[ , 1], xl[ , 2], pch = 21, bg = colors[xl[ ,3]], asp = 1, xlab = "x", ylab = "y")
 
-mu1 <- get_mu(xy1)
-mu2 <- get_mu(xy2)
+# Оценивание
+first <- xl[xl[,3] == 1,1:2]
+second <- xl[xl[,3] == 2,1:2]
+mu1 <- get_mu(first)
+mu2 <- get_mu(second)
+sigma1 <- get_matrix(first, mu1)
+sigma2 <- get_matrix(second, mu2)
+f <- get_coef(mu1,mu2,sigma1,sigma2)
 
-sigma1 <-get_matrix(xy1,mu1)
-sigma2 <-get_matrix(xy2,mu2)
+# Рисуем дискриминантую функцию
+x <- y <- seq(-10, 20, len = 100)
+z <- outer(x, y, f)
+contour(x, y, z, levels = 0, drawlabels = FALSE, lwd = 2.5, col = "green", add = TRUE)
 
-X <- seq(plotxmin-5, plotxmax+5, len = 500)
-Y <- seq(plotymin-5, plotymax+5, len = 500)
-
-f <- coef(mu1,mu2,sigma1,sigma2)
-Z <- outer(X, Y, f)
-
-contour(X, Y, Z, levels = 0, add = TRUE, drawlabels = TRUE, lwd = 2.5,col="green")
