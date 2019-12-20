62e5a4805f111ebaedf17f61f87a7ef2ebf06ef2
TIR13 <ernest_star1313@mail.ru>
2019-12-20 23:13:00 +0300

    upd adaline & heb and add log

--- line/lines.R
+++ line/lines.R
@@ -1,8 +1,8 @@
-    
 library("MASS")
+library(plotrix)
 
 normali <- function(xl) {     
-	n <- dim(xl)[2] 
+	n <- dim(xl)[2]
 	for(i in 1:n)      
 	{
 		min <- min(xl[,i])
@@ -14,14 +14,14 @@ normali <- function(xl) {
 } 
 
 loss_ada <- function(xi, yi, w) {
-	mi <- c(crossprod(w, xi)) * yi
+	mi <-sum(w*xi) * yi
 	l <- (mi - 1)^2
 	return(l)
 }
 
 # дельта правило обновления для ADALINE
 upd_ada <- function(xi, yi, w, eta) {
-	wx <- c(crossprod(w, xi))
+	wx <- sum(w * xi)
 	ld <- (wx - yi) * xi
 	W <- w - eta * ld
 	return(W)
@@ -29,7 +29,7 @@ upd_ada <- function(xi, yi, w, eta) {
 
 # Кусочно-линейную функцию потерь для Хебба
 loss_hab <- function(xi, yi, w) {
-	mi <- c(crossprod(w, xi)) * yi
+	mi <- sum(w * xi) * yi
 	return (max(-mi, 0))
 }
 
@@ -38,7 +38,20 @@ upd_hab <- function(xi, yi, w, eta) {
 	W <- w + eta * yi * xi
 	return(W)
 }
+sigmoid <- function(z) {
+    return(1 / (1 + exp(-z)))
+}
 
+loss_Log <- function(xi, yi, w) {
+     mi <- c(crossprod(w, xi)) * yi
+     l <- log2(1+exp(-mi))
+     return(l) 
+} 
+upd_Log <- function(xi, yi, w, eta) {
+     W <- w+eta*xi*yi*sigmoid(-sum(w*xi)*yi)
+     return(W)
+
+}  
 sg <- function(xl,loss,upd,coll, eta = 1, lambda = 1/6, eps = 1e-5) {     
 	l <- dim(xl)[1]     
 	n <- dim(xl)[2] - 1        
@@ -70,7 +83,7 @@ sg <- function(xl,loss,upd,coll, eta = 1, lambda = 1/6, eps = 1e-5) {
 		for (i in 1:l){         
 			xi <- xl[i, 1:n]             
 			yi <- xl[i, n + 1]                          
-			margins[i] <- crossprod(w, xi) * yi          
+			margins[i] <-  crossprod(w , xi) * yi          
 		}  
 		            
 		## select the error objects         
@@ -85,22 +98,24 @@ sg <- function(xl,loss,upd,coll, eta = 1, lambda = 1/6, eps = 1e-5) {
 			           
 			                      
 			## calculate an error             
-			ex <- loss(xi, yi, w)           
-			eta <- 1 / sqrt(sum(xi * xi))             
+			ex <- loss(xi, yi, w)
+	                eta <- 1 / sqrt(sum(xi * xi))
 			w <- upd(xi, yi, w, eta)    
-			x <- seq(-1, 2, len = 100)
-			f <- function(x) {
-				return( - x*w[1]/w[2] + w[3]/w[2] )
-			}
-			y <- f(x)
-			lines(x, y, type="l",col=coll)                      
+			if(coll!="unknown"){
+				x <- seq(-2, 2, len = 100)
+				f <- function(x) {
+					return( - x*w[1]/w[2] + w[3]/w[2] )
+				}
+				y <- f(x)
+				lines(x, y, type="l",col=coll)
+			}                      
 			## Calculate a new Q             
 			Qprev <- Q             
 			Q <- (1 - lambda) * Q + lambda * ex
-			
-			if (abs(Q - Qprev) < eps) {
+			if (abs(Q - Qprev) < 1e-5) {
 			      break
-			}         
+			} 
+     
 		}         
 		else         
 		{             
@@ -114,11 +129,11 @@ sg <- function(xl,loss,upd,coll, eta = 1, lambda = 1/6, eps = 1e-5) {
 
 n <- 100
 
-sigma1 <- matrix(c(1,0, 0, 1), 2, 2)
-sigma2 <- matrix(c(1, 0,0, 1), 2, 2)
+sigma1 <- matrix(c(2,0, 0, 2), 2, 2)
+sigma2 <- matrix(c(2, 0,0, 2), 2, 2)
 
-mu1 <- c(5, 10)
-mu2 <- c(11, 10)
+mu1 <- c(4, 4)
+mu2 <- c(12, 4)
 
 xy1 <- mvrnorm(n=n, mu = mu1, Sigma = sigma1)
 xy2 <- mvrnorm(n=n, mu = mu2, Sigma = sigma2)
@@ -142,5 +157,26 @@ ada_res <- sg(xl, loss = loss_ada, upd=upd_ada,coll="green")
 abline(a = ada_res[3] / ada_res[2], b = -ada_res[1] / ada_res[2], lwd = 3, col = "green3")
 
 hab_res <- sg(xl, loss = loss_hab, upd=upd_hab,coll="red")
-abline(a = hab_res[3] / hab_res[2], b = -hab_res[1] / hab_res[2], lwd = 3, col = "red3")  
+abline(a = hab_res[3] / hab_res[2], b = -hab_res[1] / hab_res[2], lwd = 3, col = "red3")
+
+
+plot(c(), type="n", xlab = "x", ylab = "y", xlim=c(plotxmin, plotxmax), ylim = c(plotymin, plotymax), main="LOGG")
+
+points(xl, pch=21, col=colors[ifelse(xl[,4] == -1, 1, 2)], bg=colors[ifelse(xl[,4] == -1, 1, 2)])
+Log_res <- sg(xl, loss = loss_Log, upd=upd_Log,coll="unknown")
+abline(a = Log_res[3] / Log_res[2], b = -Log_res[1] / Log_res[2], lwd = 3, col = "blue")
+
+
+ver <- function(x,w){
+	return(sigmoid(c(crossprod(w,x)) * (-1)) - sigmoid(c(crossprod(w,x)) * 1))
+}
+
+for (i in seq(len=50, plotxmin, plotxmax)) {
+	for (j in seq(len=50, plotymin, plotymax)) {
+	    p <- ver(c(i,j,-1),Log_res)
+	    coll <- adjustcolor(colors[ifelse(p>0,1,2)], abs(p))
+	    rect(i,j,i+0.05,j+0.05,col = coll, border = coll)
+  }
+}
+
 
